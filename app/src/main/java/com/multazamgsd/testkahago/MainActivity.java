dd02cde4ce238012bf76664c93367c48f69a1d29
rehanarroihan <third2014project@gmail.com>
2021-01-19 16:07:47 +0700

    login form done

--- app/src/main/java/com/multazamgsd/testkahago/MainActivity.java
+++ app/src/main/java/com/multazamgsd/testkahago/MainActivity.java
@@ -1,14 +1,41 @@
 package com.multazamgsd.testkahago;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private Dialog authDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initCustomDialog();
+    }
+
+    private void initCustomDialog(){
+        authDialog = new Dialog(MainActivity.this);
+        authDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        authDialog.setContentView(R.layout.login_form);
+        authDialog.setCancelable(true);
+
+        authDialog.show();
+        Window window = authDialog.getWindow();
+        assert window != null;
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        window.setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
     }
 }
